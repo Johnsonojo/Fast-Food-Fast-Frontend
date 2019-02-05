666ccd11f26c157cd719015974a2cac303f9a39d
Johnson Ojo <32887459+Johnsonojo@users.noreply.github.com>
2019-02-05 09:02:34 +0100

    [Feature #162853127] Place and order functionality (#10)
    [Finishes #162853127]

--- src/reducers/order.js
+++ src/reducers/order.js
@@ -1,16 +1,16 @@
-// import { POST_ORDER, POST_ORDER_PASS_MSG, POST_ORDER_FAIL_MSG } from '../actions/types';
+import { POST_ORDER, POST_ORDER_PASS_MSG, POST_ORDER_FAIL_MSG } from '../actions/types';
 
-// const initialState = { allOrder: [], message: null };
+const initialState = { allOrder: [], message: null };
 
-// export default (state = initialState, action) => {
-//   switch (action.type) {
-//     case POST_ORDER:
-//       return { ...state, allOrder: action.payload };
-//     case POST_ORDER_PASS_MSG:
-//       return { ...state, message: action.payload };
-//     case POST_ORDER_FAIL_MSG:
-//       return { ...state, message: action.payload };
-//     default:
-//       return state;
-//   }
-// };
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case POST_ORDER:
+      return { ...state, allOrder: action.payload };
+    case POST_ORDER_PASS_MSG:
+      return { ...state, message: action.payload };
+    case POST_ORDER_FAIL_MSG:
+      return { ...state, message: action.payload };
+    default:
+      return state;
+  }
+};
